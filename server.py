40e1dcb95b57073c56d51adda4e1b5ae3e52f1b4
Ömer Faruk Yanık <omerfarukyanik212313@gmail.com>
2024-01-19 02:44:09 +0300

    code fix

--- server.py
+++ server.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, request
-from gpiozero import LED
+from gpiozero import LEDBoard
 
 app = Flask(__name__)
-led1 = LED(2)
-led2 = LED(3)
-led3 = LED(4)
-led4 = LED(17)
-led5 = LED(27)
 
-leds = [led1, led2, led3, led4, led5]
+led_board = LEDBoard(5, 6, 13, 19, 26, pwm=True)
 
 
 # Web server route to control the LED
@@ -27,13 +22,11 @@ def toggle_led():
 
 
 def open_all_leds():
-    for _led in leds:
-        _led.on()
+    led_board.on()
 
 
 def close_all_leds():
-    for _led in leds:
-        _led.close()
+    led_board.close()
 
 
 def open_leds_for_slider(value):
@@ -41,9 +34,15 @@ def open_leds_for_slider(value):
         value = 5
     elif value < 0:
         value = 0
-    close_all_leds()
-    for i in range(0, 1, value):
-        leds[i].on()
+
+    _new_led_values_array = [1] * value
+    _new_led_values_array.extend([0] * (5 - value))
+
+    if value == 5:
+        led_board.value = (1, 1, 1, 1, 1)
+    elif value == 4:
+        led_board.value = (1, 1, 1, 1, 0)
+    led_board.value = tuple(_new_led_values_array)
 
 
 if __name__ == "__main__":
